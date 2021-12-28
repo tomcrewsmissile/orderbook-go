1517a86de5c3947936fef762edffeb7e7c8f29dc
tomcrewsmissile <gtm513@live.missouristate.edu>
2021-12-28 16:08:46 -0600

    added entry pool that isn't even used

--- entry.go
+++ entry.go
@@ -1,5 +1,7 @@
 package orderbookgo
 
+import "sync"
+
 
 
 type Entry struct {
@@ -12,5 +14,22 @@ type EntryStats struct {
 
 	Distance float64 //represents a percentage decimal of the distance from the head node. 
 	OrdersBetween int //the actual orders between the new order and the headnode
+
+}
+
+var entryPool = sync.Pool{}
+
+func GetEntry() *Entry {
+
+	entry := entryPool.Get()
+	if entry != nil {
+		return entry.(*Entry)
+	}
+	return &Entry{}
+}
+
+func ReleaseEntry(e *Entry) {
+
+	entryPool.Put(e)
 
 }
